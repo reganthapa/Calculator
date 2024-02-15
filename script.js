1ed2b3c429accfab58588d28507cb2d94de00d10
reganthapa <animefreaktv4@gmail.com>
2024-02-15 19:18:01 +0545

    js updated

--- script.js
+++ script.js
@@ -2,20 +2,27 @@ let input = document.getElementById("inputBox");
 let buttons = document.querySelectorAll("button");
 
 let string = "";
-buttons.forEach(button => {
-    button.addEventListener('click', (e) => {
-        if (e.target.innerHTML == "=") {
-            try {
-                string = eval(string);
-                input.value = string;
-            } catch (error) {
-                alert("Invalid expression");
-                string = "";
-                input.value = "";
-            }
-        } else {
-            string += e.target.innerHTML;
-            input.value = string;
-        }
-    });
-});
+let arr = Array.from(buttons);
+arr.forEach(button => {
+    button.addEventListener('click', (e) =>{
+      if(e.target.innerHTML == '='){
+        // eval inbuilt function
+        string = eval(string);
+        input.value = string;
+      }
+
+    else if(e.target.innerHTML == "AC"){ 
+        string = "";
+        input.value=string;
+    }
+     else if(e.target.innerHTML == "DEL"){
+        string = string.substring(0, string.length -1);
+        input.value = string;
+     }
+    else{
+        string+= e.target.innerHTML;
+      input.value = string;
+    }
+
+    })
+})
